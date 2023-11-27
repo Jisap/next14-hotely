ca342b31e3482c76282048aeaf00a246c92c1a09
Jorge Matos <jisapjisap@gmail.com>
2023-11-27 16:22:19 +0100

    SearchBox

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,11 @@
+import SearchBox from "./SearchBox"
 
 
 const Hero = () => {
   return (
     <section className="pt-24 pb-12 xl:py-0 xl:h-[1087px] bg-hero2 xl:bg-hero
     bg-center bg-no-repeat flex justify-center items-center">
-      Hero
+      <SearchBox />
     </section>
   )
 }
