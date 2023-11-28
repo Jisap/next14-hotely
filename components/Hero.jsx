fabbd8f0bf74e713827abdd5059186451843a6f9
Jorge Matos <jisapjisap@gmail.com>
2023-11-28 16:25:33 +0100

    searchbox

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,9 @@ const Hero = () => {
   return (
     <section className="pt-24 pb-12 xl:py-0 xl:h-[1087px] bg-hero2 xl:bg-hero
     bg-center bg-no-repeat flex justify-center items-center">
-      <SearchBox />
+      <div className="container mx-auto flex items-center justify-center xl:justify-start">
+        <SearchBox />
+      </div>
     </section>
   )
 }
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+'use client'
+
+import { MapPin } from "lucide-react"
+import { Button } from "./ui/button"
+import { Checkbox } from "./ui/checkbox"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+import { motion } from "framer-motion"
+import { fadeIn } from "@/variants"
+import DatePicker from "./DatePicker"
+
+
+
+
 
 const SearchBox = () => {
   return (
-    <div>SearchBox</div>
+    <div>
+      {/* input, calendar, date picker */}
+      <div>
+        {/* label */}
+        <Label htmlFor='destination'>Where are you going ?</Label>
+        {/* input & icon */}
+        <div className="relative flex items-center mb-[20px]">
+          <Input id='destination' placeholder='Try "Singapore"' />
+          <MapPin size={24} className="absolute right-6 text-black" />
+        </div>
+        {/* date pickers */}
+        <div>
+          {/* check in */}
+          <div>
+            <Label>Check in</Label>
+            <DatePicker />
+          </div>
+          {/* check out */}
+          <div>
+            <Label>Check out</Label>
+            <DatePicker />
+          </div>
+        </div>
+      </div>
+
+      {/* select, checkbox, btn */}
+
+    </div>
   )
 }
 
