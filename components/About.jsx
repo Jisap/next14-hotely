a982010d5e4b8d83748f17d4d8a14e96a632f8bc
Jorge Matos <jisapjisap@gmail.com>
2023-12-01 19:34:54 +0100

    About, Stats

--- components/About.jsx
+++ components/About.jsx
@@ -12,11 +12,11 @@ import Stats from "./Stats"
 
 const About = () => {
   return (
-    <section >
+    <section className="py-12 xl:pt-10 xl:pb-24">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row">
           {/* img */}
-          <div>
+          <div className="flex-1 relative">
             <Image 
               src={'/about/img2.png'}
               width={559}
@@ -25,7 +25,7 @@ const About = () => {
             />
           </div>
           {/* text */}
-          <div>
+          <div className="xl:max-w-[470px]">
             <h2 className="h2 mb-[38px]">About Hotely</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
@@ -34,7 +34,7 @@ const About = () => {
               irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </p>
             {/* stats */}
-            <div className="bg-pink my-5 xl:my-10 min-h-[35px]">
+            <div className="my-5 xl:my-10 min-h-[35px]">
               <Stats />
             </div>
             <p className="mb-10">
