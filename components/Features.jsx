2be34b0621f21fe582ff152572d720cc816868a2
Jorge Matos <jisapjisap@gmail.com>
2023-12-02 16:09:17 +0100

    Features motion

--- components/Features.jsx
+++ components/Features.jsx
@@ -11,7 +11,7 @@ const Features = () => {
       variants={fadeIn('right', 0.2)}
       initial="hidden"
       whileInView={"show"}
-      viewport={{ once: false, amount: 0.4 }}
+      viewport={{ once: false, amount: 0.2 }}
       className="bg-soft_green-secondary xl:h-[240px] max-w-[1305px] ml-auto
       xl:-top-[120px] relative rounded-tl-[30px] rounded-bl-[30px] px-[80px] py-[60px]"
     >
