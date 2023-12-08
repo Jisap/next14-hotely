b738ff2dc2b56a88210a5aef75be3a1f3d6f46d6
Jorge Matos <jisapjisap@gmail.com>
2023-12-08 18:57:33 +0100

    Testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -3,6 +3,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css'
+import 'swiper/css/pagination';
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -17,7 +18,7 @@ const TestimonialData = [
   },
   {
     img: '/testimonials/img-2.png',
-    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     personName: 'Mario Rupertinno',
     location: 'Roma'
   },
@@ -26,8 +27,20 @@ const TestimonialData = [
 
 const Testimonials = () => {
   return (
-    <section className="bg-soft_green xl:h-[880px]">
-      <div className="container mx-auto">
+    <motion.section 
+      variants={fadeIn('up', 0.2)}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0 }}  
+      className="bg-soft_green xl:h-[880px]"
+    >
+      <motion.div 
+        variants={fadeIn('up', 0.4)}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0 }}
+        className="container mx-auto"
+      >
         <Swiper
           modules={[Pagination]}
           pagination={{clickable: true}}
@@ -55,8 +68,8 @@ const Testimonials = () => {
             )
           })}
         </Swiper>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   )
 }
 
